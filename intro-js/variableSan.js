16fb668b398b84b3f52d9891807312607ae26364
Sandra <sandralu317@hotmail.com>
2021-08-27 21:52:50 -0500

    New exercise

--- intro-js/variableSan.js
+++ intro-js/variableSan.js
@@ -1,27 +1,37 @@
-/*const nombreEstudiante = prompt("Ingrese su nombre por favor");
 
-console.log(nombreEstudiante);*/
 const nombre = prompt("Ingrese su nombre");
 const edad = Number(prompt("Ingrese edad"));
-
-/*if(!Number.isNaN(edad)){
-    //No ingresó un numero
-    alert(`El(la) joven ${nombre} tiene ${edad}  años`);
-}else{
-    alert("No ingresó numero");
-}*/
 
 // pregunten la edad de un usuario y si es menor de edad digan q no puede acceder al sitio
 // si es mayor de edad, un mensaje de bienvenida con el nombre
 // si no ingresa un numero un mensaje de error o advertencia
 
-if(!Number.isNaN(edad)){
+if(!isNaN(edad)){
     //No ingresó un numero
     if(edad > 18){
-        alert(`Bienvenido ${nombre} usted es mayo de edad!`);
+        alert(`Bienvenido ${nombre} usted es mayor de edad!`);
+
+        const numero = Number(prompt("Ingrese un número"));
+
+        if(!isNaN(numero)){
+
+            //Pregunte por un numero al usuario o muestren si es par o impar "el numero x es ???"
+            // en caso de no ingresar un numero, error"
+            if(numero %2 == 0){
+                alert(`El número ${numero} es par`);
+            }else{
+                alert(`El número ${numero} es impar`);
+            }
+        }else{
+            alert("No ingresó un numero");
+        }
     }else{
-        alert(`${nombre} no puedes acceder a este sitio!`);
+        alert(`${nombre}, eres menor de edad y no puedes acceder a este sitio!`);
     }
 }else{
     alert("No ingresó numero");
 }
+
+
+
+
