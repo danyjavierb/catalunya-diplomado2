ba443c8fff710d8573e7f375dcfdb93f0496dab7
Dany Bautista <danyjavierb@gmail.com>
2021-09-04 11:31:37 -0500

    intro objetos y ejercicios dom soluciones

--- intro-dom/dom.js
+++ intro-dom/dom.js
@@ -104,6 +104,76 @@ const programadores = [
 
 // iterar sobre programadores y vamos a pintar un elemento
 // h1 para cada nombre de programador
+// camilo barbosa
+function printNames(list) {
+  const container = document.createElement("div");
+  list.forEach((prog) => {
+    const progElement = document.createElement("div");
+    progElement.innerHTML = `
+        <h1>${prog.nombre}</h1>
+        <ul>${prog.lenguajes.reduce(
+          (ac, lang) => ac + `<li>${lang}</li>`,
+          ""
+        )} </ul>
+        `;
+    container.appendChild(progElement);
+  });
+  return container;
+}
+//title.appendChild(printNames(programadores));
+
+// edwin
+
+// programadores.forEach((element) => {
+//   const nuevoElementoprogramador = document.createElement("div");
+//   nuevoElementoprogramador.innerHTML = `<h1>${element.nombre}</h1>`;
+//   title.appendChild(nuevoElementoprogramador);
+// });
+
+//wilmer
+
+const nombresProgramadores = (array) => {
+  const div1 = document.querySelector("#title");
+  array.forEach((element) => {
+    let h1_div = document.createElement("div");
+    h1_div.innerHTML = `<h1>${element.nombre}</h1><br>`;
+    div1.appendChild(h1_div);
+  });
+};
+
+//camilo
+
+const getDevNames = (array) => {
+  let result = [];
+  for (let index = 0; index < array.length; index++) {
+    const nuevoElemento = document.createElement("div");
+    nuevoElemento.innerHTML = `<h1 > ${array[index].nombre}</h1>`;
+
+    result.push(nuevoElemento);
+  }
+  return result;
+};
+getDevNames(programadores).forEach((elem) => {
+  title.appendChild(elem);
+});
+
+// jose
+const CreateList = (array) => {
+  const bodyElement = document.querySelector("body");
+  const newDiv = document.createElement("div");
+  const elements = array.map((element) => {
+    return `<h1>${
+      element.nombre
+    }</h1><ul><span>Lenguajes</span>${element.lenguajes
+      .map((el) => `<li>${el}</li>`)
+      .join("")}</ul>`;
+  });
+  newDiv.innerHTML = elements.join("");
+  bodyElement.appendChild(newDiv);
+};
+CreateList(programadores);
+
+// nombresProgramadores(programadores);
 
 // iterar sobre programadores y
 /**
@@ -119,3 +189,37 @@ ID | nombre | lengs | edad | perfil
 1     jose     ...     ...  
 
 */
+
+// Andres Gallo
+//https://jsfiddle.net/a6wszhx0/2/
+
+//jose
+
+const createTable = (array) => {
+  const bodyElement = document.querySelector("body");
+  const newDiv = document.createElement("div");
+  const table = document.createElement("table");
+  if (array.length == 0)
+    return bodyElement.appendChild("<h2>The array don't have elements</h2>");
+  const headerText = Object.keys(array[0]);
+  const tableHead = `<tr>${headerText
+    .map((x) => `<th>${x}</th>`)
+    .join("")}</tr>`;
+  const tableBody = array
+    .map((element) => {
+      return `<tr>${headerText
+        .map(
+          (x) =>
+            `<td>${
+              typeof element[x] == "function" ? element[x]() : element[x]
+            }</td>`
+        )
+        .join("")}</tr>`;
+    })
+    .join("");
+  table.innerHTML = tableHead.concat(tableBody);
+  newDiv.appendChild(table);
+  bodyElement.appendChild(newDiv);
+};
+
+createTable(programadores);
