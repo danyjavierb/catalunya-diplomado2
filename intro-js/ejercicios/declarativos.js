f9975114ada18ab36c8055bf0fcb205e429a971a
Jose Moreno <jmoreno@ias.com.co>
2021-09-04 08:18:19 -0500

    [UPD]Tarea pendiente.

--- intro-js/ejercicios/declarativos.js
+++ intro-js/ejercicios/declarativos.js
@@ -57,5 +57,112 @@ console.log(GetNames(programadores)); */
 
 console.log(SumEdades(programadores)); */
 
-// Ejercicios
+//Ejercios
+
+const productos = [
+    {
+      id: 1,
+      nombre: "papitas francesas",
+      precio: 5000,
+    },
+    {
+      id: 2,
+      nombre: "hamburguesa",
+      precio: 10000,
+    },
+    {
+      id: 3,
+      nombre: "malteada",
+      precio: 8000,
+    },
+  ];
+  
+  const ordenes = [
+    {
+      id: 1,
+      idUsuario: 1,
+      productos: [
+        {
+          idProducto: 1,
+          cantidad: 1,
+        },
+        {
+          idProducto: 3,
+          cantidad: 2,
+        },
+      ],
+    },
+    {
+      id: 2,
+      idUsuario: 3,
+      productos: [
+        {
+          idProducto: 1,
+          cantidad: 1,
+        },
+        {
+          idProducto: 2,
+          cantidad: 1,
+        },
+        {
+          idProducto: 3,
+          cantidad: 1,
+        },
+      ],
+    },
+  ];
+  
+  // ejercicios
+  
+  // escribir una funcion que retorne orden con precio total
+  // calcularPrecioOrden (idOrden) //2 {
+  //  id: 2
+  // precioTotal: 23000
+  //}
+
+/*   const GetTotalOrder = (array, id) => {
+    const order = array.find((element) => element.id == id);
+    return order.productos.reduce((acc, curr) => {
+        product = productos.find((prod) => prod.id == curr.idProducto);
+        return acc += (product.precio * curr.cantidad);
+    }, 0) 
+  }
+  
+  console.log(GetTotalOrder(ordenes, 2))
+ */
+  // una funcion que retornde los pedidos de un usuario
+  // traerPedidosUsario (idUsuario)
+  // [
+  //     {
+  //         id: 2,
+  //         idUsuario: 3,
+  //         productos: [{
+  //             cantidad:1
+  //             producto : {
+  //                 ...
+  //             }
+  //         }]
+  //     }
+  // ]
+/* 
+  const GetOrderByUser = (array, id) => {
+    return array.filter((order) => order.idUsuario == id).map(
+        (element) => {
+            return {
+                ...element,
+                productos: productos.map((prod) => prod.id == element.productos.find((el) => el.idProducto == prod.id))
+            }
+        }
+    )
+}
+
+console.log(GetOrderByUser(ordenes, 3))
+   */
+  //funcion que retorne el promedio de gasto de un usuario
+
+  
+  // funcion que retorne el producto mas pedido
+  
+  //  funcion que retorne el usuario que mas ha gastado en nuestra app
+  
 
