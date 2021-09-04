ce5af3f58f37affcf3d861a7a3fd7bb56c502451
Dany Bautista <danyjavierb@gmail.com>
2021-09-04 12:43:08 -0500

    feat: iniciando codigo todo-list

---
+++ ejercicio-todo-list/todo-list.js
@@ -0,0 +1,16 @@
+const todos = [
+  {
+    id: 1,
+    todo: "ir a pasear a los perros",
+    completado: false,
+  },
+  {
+    id: 2,
+    todo: "ir a pagar la luz",
+    completado: false,
+  },
+];
+
+const pintarTodos = () => {
+  // repintado
+};
